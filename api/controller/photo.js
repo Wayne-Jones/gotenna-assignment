caa6c8268a6744efe4a2d3a0afd3b5e099ec5275
Wayne Jones <waynejones@nyu.edu>
2020-05-19 02:54:07 -0400

    Added Controller function for Modifying Photos and getting by UID

--- api/controller/photo.js
+++ api/controller/photo.js
@@ -4,7 +4,12 @@ const getPhotos = (req, res) => {
     
     const paginationLimit = req.query.pagination ? parseInt(req.query.pagination) : 20;
     const page = req.query.page ? parseInt(req.query.page) : 1;
-    Photo.find({}, (err, photo) =>{
+    const widthLower = req.query.widthLower ? parseInt(req.query.widthLower) : 0;
+    const widthHigher = req.query.widthHigher ? parseInt(req.query.widthHigher) : Number.MAX_SAFE_INTEGER;
+    const heightLower = req.query.heightLower ? req.query.heightLower : 0;
+    const heightHigher = req.query.heightHigher ? req.query.heightHigher : Number.MAX_SAFE_INTEGER;
+
+    Photo.find({ width: { $gte: widthLower, $lte: widthHigher }, height: { $gte: heightLower, $lte: heightHigher } }, (err, photo) =>{
         if(err){
             res.send(err);
         }
@@ -12,6 +17,18 @@ const getPhotos = (req, res) => {
     })
     .skip((page - 1) * paginationLimit)
     .limit(paginationLimit)
+};
+
+const modifyPhoto = (req, res) => {
+    
+    const greyscale = (req.query.greyscale ==  'true') ? true : false;
+
+    Photo.updateMany({greyscale: !greyscale}, {$set: {greyscale: greyscale}}, (err, photo) =>{
+        if(err){
+            res.send(err);
+        }
+        res.json(photo);
+    })
 };
 
 const addPhoto = (req, res) => {
@@ -24,4 +41,14 @@ const addPhoto = (req, res) => {
     })
 };
 
-module.exports = {getPhotos, addPhoto};
+const getPhotoByUID = (req, res) => {
+
+    Photo.find({_id: req.params.id}, (err, photo) =>{
+        if (err){
+            res.send(err);
+        }
+        res.json(photo);
+    })
+};
+
+module.exports = {getPhotos, addPhoto, getPhotoByUID, modifyPhoto};
