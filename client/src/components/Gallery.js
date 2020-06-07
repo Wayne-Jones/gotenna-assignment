f677c42e2fcdea5030ae781f391e701bb541b82c
Wayne Jones <waynejones@nyu.edu>
2020-06-07 15:51:02 -0400

    Width and Height Sliders are working

--- client/src/components/Gallery.js
+++ client/src/components/Gallery.js
@@ -1,17 +1,23 @@
 import React, { useState, useEffect } from 'react';
 import ReactGallery from 'react-grid-gallery';
-import { Grid } from '@material-ui/core';
+import { Grid, Slider, Typography } from '@material-ui/core';
 import { Pagination } from '@material-ui/lab';
 
 function Gallery() {
   const [imgObj, setImgObj] = useState([]);
   const [galleryImg, setGalleryImg] = useState([]);
   const [page, setPage] = useState(1);
+  const [widthRange, setWidthRange] = useState([0, 500]);
+  const [heightRange, setHeightRange] = useState([0, 500]);
 
   const fetchImages = () => {
     let url = 'http://localhost:4000/api/v0/photo';
     const pageUrlString = `?page=${page}`;
+    const widthUrlString = `&widthLower=${widthRange[0]}&widthHigher=${widthRange[1]}`;
+    const heightUrlString = `&heightLower=${heightRange[0]}&heightHigher=${heightRange[1]}`;
     url = url.concat(pageUrlString);
+    url = url.concat(widthUrlString);
+    url = url.concat(heightUrlString);
     fetch(url)
       .then((resp) => resp.json())
       .then((data) => {
@@ -26,6 +32,12 @@ function Gallery() {
 
   const loadPage = (currentPage) => {
     setPage(currentPage);
+  };
+  const loadWidthFilter = (filterRange) => {
+    setWidthRange(filterRange);
+  };
+  const loadHeightFilter = (filterRange) => {
+    setHeightRange(filterRange);
   };
   const formatObj = (img) => {
     const {
@@ -45,13 +57,32 @@ function Gallery() {
   };
   useEffect(() => {
     fetchImages();
-  }, [page]);
+  }, [page, widthRange, heightRange]);
 
   return (
     <>
-      <Grid container>
+      <Grid container spacing={3}>
         <Grid item xs={12}><ReactGallery images={galleryImg} /></Grid>
-        <Grid item xs={12}><Pagination count={3} onChange={(event, page) => loadPage(page)} /></Grid>
+        <Grid container item xs={12}><Pagination count={3} onChange={(event, currentPage) => loadPage(currentPage)} /></Grid>
+        <Grid container item xs={6}>
+          <Typography gutterBottom>Width Filter Range</Typography>
+          <Slider
+            value={widthRange}
+            onChangeCommitted={(event, rangeArray) => loadWidthFilter(rangeArray)}
+            valueLabelDisplay="auto"
+            max={500}
+          />
+        </Grid>
+        <Grid container item xs={6}>
+          <Typography gutterBottom>Height Filter Range</Typography>
+          <Slider
+            value={heightRange}
+            onChangeCommitted={(event, rangeArray) => loadHeightFilter(rangeArray)}
+            valueLabelDisplay="auto"
+            max={500}
+          />
+
+        </Grid>
       </Grid>
     </>
   );
